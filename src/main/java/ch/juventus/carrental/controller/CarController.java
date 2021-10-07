48525ecaeb5fd65fb2ba048a434a3f1c32f9c775
didierengorokere <86884516+didierengorokere@users.noreply.github.com>
2021-10-07 20:51:49 +0200

    Hello World über HttpStatus

--- src/main/java/ch/juventus/carrental/controller/CarController.java
+++ src/main/java/ch/juventus/carrental/controller/CarController.java
@@ -1,8 +1,16 @@
 package ch.juventus.carrental.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CarController {
+    @GetMapping("/api/v1/helloWorld")
+    public ResponseEntity<String> helloworld() {
+        String response = "hello world";
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 
 }

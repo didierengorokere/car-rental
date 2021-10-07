4504c01249ad98635a48215f165e5aeb5ccab7d8
didierengorokere <86884516+didierengorokere@users.noreply.github.com>
2021-10-07 21:13:29 +0200

    Corrections packages out of the ch.juventus fixed

--- src/main/java/ch/juventus/carrental/controller/CarController.java
+++ src/main/java/ch/juventus/carrental/controller/CarController.java
@@ -4,12 +4,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ch.juventus.carrental.service.CarService;
 
 @RestController
 public class CarController {
+
+    private final CarService carService;
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
     @GetMapping("/api/v1/helloWorld")
     public ResponseEntity<String> helloworld() {
-        String response = "hello world";
+        String response = carService.getHelloWorldGreeting();
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
---
+++ src/main/java/ch/juventus/carrental/service/CarService.java
@@ -0,0 +1,11 @@
+package ch.juventus.carrental.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CarService {
+
+    public String getHelloWorldGreeting() {
+        return "hello World";
+    }
+}
